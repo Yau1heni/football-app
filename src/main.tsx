2093cda9a8b7695c7cf921807e0addb375447c93
Yau1heni <lodbrok9@gmail.com>
2026-02-24 19:16:52 +0300

    feat: install and configure tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,26 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ErrorBoundary } from 'components/error-boundary';
 import { routesConfig } from 'configs/routes-config.tsx';
+import { clientOptions } from 'configs/tanstack-query-config.ts';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router';
-
 import 'styles/styles.scss';
 
 const router = createBrowserRouter(routesConfig);
 
 const root = createRoot(document.getElementById('root') as HTMLDivElement);
 
+const queryClient = new QueryClient(clientOptions);
+
 root.render(
   <StrictMode>
     <ErrorBoundary>
-      <RouterProvider router={router} />
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={router} />
+        <ReactQueryDevtools />
+      </QueryClientProvider>
     </ErrorBoundary>
   </StrictMode>
 );
