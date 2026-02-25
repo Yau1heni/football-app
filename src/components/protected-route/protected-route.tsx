b1f9d5e71ae310eaaa03228e09fe271e8b813996
Yau1heni <lodbrok9@gmail.com>
2026-02-25 21:53:05 +0300

    refactor: display protected-route loading status

--- src/components/protected-route/protected-route.tsx
+++ src/components/protected-route/protected-route.tsx
@@ -1,12 +1,21 @@
+import { Loader } from 'components/ui/loader';
 import { routes } from 'configs/routes.ts';
 import { useAuthContext } from 'contexts/auth';
 import type { FC, PropsWithChildren } from 'react';
 import { Navigate } from 'react-router';
 
+import styles from './protected-route.module.scss';
+
 export const ProtectedRoute: FC<PropsWithChildren> = ({ children }) => {
   const { isAuthenticated, isLoading } = useAuthContext();
 
-  if (isLoading) return null;
+  if (isLoading) {
+    return (
+      <div className={styles.loaderWrapper}>
+        <Loader />
+      </div>
+    );
+  }
 
   return isAuthenticated ? <>{children}</> : <Navigate to={routes.login.mask} />;
 };
